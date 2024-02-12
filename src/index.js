fb2f71c37cd476ebe24a6bf3201fd3951ace8937
DadaBabii <107479813+DadaBabii@users.noreply.github.com>
2024-02-12 14:55:43 +0200

    1-task

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import './styles/index.css';
 import { renderAllProducts } from './js/renderMarkup';
 import { refs } from './js/refs';
 import { getProductById } from './requests/products';
+import { getAllProducts } from './requests/products';
 import { createMarkupProductById } from './services/markupService';
 // renderAllProducts();
+
+refs.getAllProdBtn.addEventListener('click', onclickAllProdacts);
+
+function onclickAllProdacts(e) {
+  e.preventDefault();
+  getAllProducts();
+  renderAllProducts();
+};
+
 
 refs.singleProductForm.addEventListener('submit', onFormIdSubmit);
 
@@ -20,3 +30,4 @@ async function onFormIdSubmit(e) {
   }
   e.target.reset;
 }
+console.log(refs.productsList);
